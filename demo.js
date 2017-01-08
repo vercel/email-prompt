29be52baa6c7dd692c1fd9789e44d799f524b543
Leo Lamprecht <mindrun@icloud.com>
2017-01-08 22:59:18 +0100

    Lint using XO

--- demo.js
+++ demo.js
@@ -1,7 +1,7 @@
 require('./build')()
-.then((email) => {
-  console.log('\n> Thanks ' + email);
+.then(email => {
+  console.log('\n> Thanks ' + email)
 })
 .catch(() => {
-  console.log('\n> Aborted! Bye');
-});
+  console.log('\n> Aborted! Bye')
+})
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const ansi = require('ansi-escapes');
-const chalk = require('chalk');
+const ansi = require('ansi-escapes')
+const chalk = require('chalk')
 
 module.exports = exports.default = function emailPropt({
   start = '> Enter your email: ',
@@ -30,105 +30,106 @@ module.exports = exports.default = function emailPropt({
   allowInvalidChars = false
 } = {}) {
   return new Promise((resolve, reject) => {
-    const isRaw = process.stdin.isRaw;
-    const isPaused = process.stdin.isPaused();
+    const isRaw = process.stdin.isRaw
 
-    process.stdout.write(start);
-    process.stdin.setRawMode(true);
-    process.stdin.resume();
+    process.stdout.write(start)
+    process.stdin.setRawMode(true)
+    process.stdin.resume()
 
-    let val = '';
-    let suggestion = '';
-    let caretOffset = 0;
+    let val = ''
+    let suggestion = ''
+    let caretOffset = 0
 
     // to make `for..of` work with buble
-    const _domains = Array.from(domains);
+    const _domains = Array.from(domains)
 
-    const ondata = (v) => {
-      const s = v.toString();
+    const ondata = v => {
+      const s = v.toString()
 
       // abort upon ctrl+C
       if (abortChars.has(s)) {
-        restore();
-        return reject(new Error('User abort'));
+        restore()
+        return reject(new Error('User abort'))
       }
 
-      let completion = '';
+      let completion = ''
 
       // if we have a suggestion *and*
       // the user is at the end of the line *and*
       // the user pressed one of the keys to trigger completion
       if (suggestion !== '' && !caretOffset && autoCompleteChars.has(s)) {
-        val += suggestion;
-        suggestion = '';
+        val += suggestion
+        suggestion = ''
       } else {
-        if ('\x1b[D' === s) {
+        if (s === '\x1b[D') {
           if (val.length > Math.abs(caretOffset)) {
-            caretOffset--;
+            caretOffset--
           }
-        } else if ('\x1b[C' === s) {
+        } else if (s === '\x1b[C') {
           if (caretOffset < 0) {
-            caretOffset++;
+            caretOffset++
           }
-        } else if ('\x08' === s || '\x7f' === s) {
+        } else if (s === '\x08' || s === '\x7f') {
           // delete key needs splicing according to caret position
           val = val.substr(0, val.length + caretOffset - 1) +
-            val.substr(val.length + caretOffset);
+            val.substr(val.length + caretOffset)
         } else {
           if (resolveChars.has(s)) {
-            restore();
-            return resolve(val);
+            restore()
+            return resolve(val)
           }
 
           if (!allowInvalidChars) {
             // disallow more than one @
-            if (/@/.test(val) && '@' === s) {
-              return;
+            if (/@/.test(val) && s === '@') {
+              return
             }
 
             if (/[^A-z0-9-+_.@]/.test(s)) {
-              return;
+              return
             }
           }
 
-          const add = forceLowerCase ? s.toLowerCase() : s;
+          const add = forceLowerCase ? s.toLowerCase() : s
           val = val.substr(0, val.length + caretOffset) + add +
-            val.substr(val.length + caretOffset);
+            val.substr(val.length + caretOffset)
         }
 
-        const parts = val.split('@');
-        if (2 === parts.length && parts[1].length) {
-          const [, _host] = parts;
-          const host = _host.toLowerCase();
+        const parts = val.split('@')
+        if (parts.length === 2 && parts[1].length > 0) {
+          const [, _host] = parts
+          const host = _host.toLowerCase()
           for (const domain of _domains) {
             if (host === domain) {
-              break;
+              break
             }
 
             if (host === domain.substr(0, host.length)) {
-              suggestion = domain.substr(host.length);
-              completion = chalk[suggestionColor](suggestion);
-              completion += ansi.cursorBackward(domain.length - host.length);
-              break;
+              suggestion = domain.substr(host.length)
+              completion = chalk[suggestionColor](suggestion)
+              completion += ansi.cursorBackward(domain.length - host.length)
+              break
             }
           }
         }
 
-        if (!completion.length) suggestion = '';
+        if (completion.length > 0) {
+          suggestion = ''
+        }
       }
 
-      process.stdout.write(ansi.eraseLines(1) + start + val + completion);
+      process.stdout.write(ansi.eraseLines(1) + start + val + completion)
       if (caretOffset) {
-        process.stdout.write(ansi.cursorBackward(Math.abs(caretOffset)));
+        process.stdout.write(ansi.cursorBackward(Math.abs(caretOffset)))
       }
-    };
+    }
 
     const restore = () => {
-      process.stdin.setRawMode(isRaw);
-      process.stdin.pause();
-      process.stdin.removeListener('data', ondata);
-    };
+      process.stdin.setRawMode(isRaw)
+      process.stdin.pause()
+      process.stdin.removeListener('data', ondata)
+    }
 
-    process.stdin.on('data', ondata);
-  });
+    process.stdin.on('data', ondata)
+  })
 }
